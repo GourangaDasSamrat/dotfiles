54c3ec7ccd3d8ddd1ad75a0adfa69c9d55127893
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-16 16:11:14 +0600

    chore: Refactor install_tools.sh for clarity
    - Moved post-installation setup to a common section
    - Improved OS unsupported message handling
    - Ensured consistent installation checks

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -19,6 +19,7 @@ if [ "$OS" = "Darwin" ]; then
 			brew install "$tool"
 		fi
 	done
+
 elif [ "$OS" = "Linux" ]; then
 	# Add zsh to the tools list for Linux
 	TOOLS+=("zsh")
@@ -58,37 +59,40 @@ elif [ "$OS" = "Linux" ]; then
 		done
 	fi
 
-	# Post-installation commands for Linux
-	if [ -d "$HOME/.oh-my-zsh" ]; then
-		echo "Oh My Zsh is already installed, skipping..."
-	else
-		echo "Installing Oh My Zsh..."
-		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-	fi
-
-	if [ -d "$HOME/.tmux/plugins/tpm" ]; then
-		echo "TPM is already installed, skipping..."
-	else
-		echo "Installing TPM (Tmux Plugin Manager)..."
-		git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-	fi
-
-	if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-		echo "zsh-autosuggestions is already installed, skipping..."
-	else
-		echo "Installing zsh-autosuggestions..."
-		git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-	fi
-
-	if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
-		echo "zsh-syntax-highlighting is already installed, skipping..."
-	else
-		echo "Installing zsh-syntax-highlighting..."
-		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-	fi
 else
 	echo "Unsupported operating system: $OS"
 	exit 1
+fi
+
+# Post-installation setup (common for both macOS and Linux)
+echo "Running post-installation setup..."
+
+if [ -d "$HOME/.oh-my-zsh" ]; then
+	echo "Oh My Zsh is already installed, skipping..."
+else
+	echo "Installing Oh My Zsh..."
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+if [ -d "$HOME/.tmux/plugins/tpm" ]; then
+	echo "TPM is already installed, skipping..."
+else
+	echo "Installing TPM (Tmux Plugin Manager)..."
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+	echo "zsh-autosuggestions is already installed, skipping..."
+else
+	echo "Installing zsh-autosuggestions..."
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+if [ -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+	echo "zsh-syntax-highlighting is already installed, skipping..."
+else
+	echo "Installing zsh-syntax-highlighting..."
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
 # Run stow command in ~/dotfiles directory
