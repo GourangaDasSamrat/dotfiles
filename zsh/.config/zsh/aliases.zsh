8965defd215c58e67faf898fb509814dc2be91fc
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-02-23 15:09:11 +0600

    refactor(zsh): Improve alias definitions
    - Add conditional aliases for fd and bat
    - Make 'code' alias check for installed VS Code
    - Ensure 'eza' aliases are used when available
    - Improve 'la' alias for eza

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,12 +2,29 @@ alias debian=TERM='xterm-256color proot-distro login debian --user gouranga'
 alias df='cd ~/../usr/var/lib/proot-distro/installed-rootfs/debian/home/gouranga/'
 alias af='cd ~/storage/shared'
 
-alias ls='eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions'
-alias lt='eza --tree -a -I "node_modules|.git"'
-alias la='ls -A'
+# Conditional Aliases
+(( $+commands[fdfind] )) && alias fd='fdfind'
+(( $+commands[batcat] )) && alias bat='batcat'
 
-alias code=code-oss
-alias code-b='code --profile "Backend Dev"'
-alias code-c='code --profile "C/C++ Dev"'
-alias code-f='code --profile "Frontend Dev"'
-alias code-g='code --profile "Go Dev"'
+if (( $+commands[code-oss] )); then
+    alias code='code-oss'
+elif (( $+commands[code-insiders] )); then
+    alias code='code-insiders'
+fi
+
+# Eza's Aliases
+if (( $+commands[eza] )); then
+    alias ls='eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions'
+    alias lt='eza --tree -a -I "node_modules|.git"'
+    alias la='eza -la --icons=always'
+else
+    alias la='ls -A'
+fi
+
+# Vscode's Aliases
+if (( $+commands[code] )) || alias code >/dev/null 2>&1; then
+    alias code-b='code --profile "Backend Dev"'
+    alias code-c='code --profile "C/C++ Dev"'
+    alias code-f='code --profile "Frontend Dev"'
+    alias code-g='code --profile "Go Dev"'
+fi
