76f4452b61db9bee12ab10b3b384dc9764d8d137
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-03-31 18:06:20 +0600

    feat(zsh): add environment configuration
    - Export DOTFILES, UV_LINK_MODE, BAT_THEME
    - Add $HOME/go/bin to PATH

---
+++ zsh/.config/zsh/env.zsh
@@ -0,0 +1,14 @@
+# Dotfiles
+export DOTFILES="$HOME/dotfiles"
+
+# UV
+export UV_LINK_MODE=copy
+
+# BAT
+export BAT_THEME="Dracula"
+
+# Path
+go_bin="$HOME/go/bin"
+
+export PATH="$go_bin:$PATH"
+unset go_bin
