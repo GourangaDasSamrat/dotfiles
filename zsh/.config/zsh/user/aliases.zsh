e0107d5e4c7aaeb0445220a58a436ea0a2d01051
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-04-03 17:45:07 +0600

    refactor(zsh): Improve VS Code alias logic
    - Consolidate VS Code alias detection
    - Add default profile for 'code' alias
    - Ensure 'code' alias works correctly

--- zsh/.config/zsh/user/aliases.zsh
+++ zsh/.config/zsh/user/aliases.zsh
@@ -9,12 +9,6 @@ fi
 (($+commands[fdfind])) && alias fd='fdfind'
 (($+commands[batcat])) && alias bat='batcat'
 
-if (($+commands[code-oss])); then
-  alias code='code-oss'
-elif (($+commands[code-insiders])); then
-  alias code='code-insiders'
-fi
-
 # --- Navigation and Config Aliases ---
 if [[ -d "$DOTFILES" ]]; then
   alias dot='cd "$DOTFILES"'
@@ -37,13 +31,21 @@ fi
 alias la='ls -A'
 
 # --- Vscode's Aliases ---
-if (($+commands[code])) || alias code >/dev/null 2>&1; then
-  alias code-b='code --profile "Backend Dev"'
-  alias code-c='code --profile "C/C++ Dev"'
-  alias code-d='code --profile "Database Dev"'
-  alias code-f='code --profile "Frontend Dev"'
-  alias code-g='code --profile "Go Dev"'
-  alias code-w='code --profile "Wiki Dev"'
+if (($+commands[code-oss])); then
+    alias code='code-oss --profile Default'
+elif (($+commands[code-insiders])); then
+    alias code='code-insiders --profile Default'
+elif (($+commands[code])); then
+    alias code='command code --profile Default'
+fi
+
+if alias code >/dev/null 2>&1 || (($+commands[code])); then
+    alias code-b='code --profile "Backend Dev"'
+    alias code-c='code --profile "C/C++ Dev"'
+    alias code-d='code --profile "Database Dev"'
+    alias code-f='code --profile "Frontend Dev"'
+    alias code-g='code --profile "Go Dev"'
+    alias code-w='code --profile "Wiki Dev"'
 fi
 
 # --- Go' Aliases ---
