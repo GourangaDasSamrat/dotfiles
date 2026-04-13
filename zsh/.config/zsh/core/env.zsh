09702f494c5f76a3d36717395af16ea6c69945f0
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-04-13 16:55:26 +0600

    feat(env): Add PNPM to environment variables
    - Added PNPM_HOME export
    - Included PNPM_HOME in PATH

--- zsh/.config/zsh/core/env.zsh
+++ zsh/.config/zsh/core/env.zsh
@@ -1,25 +1,28 @@
 # --- Environment Variables ---
-export DOTFILES="$HOME/dotfiles" # Path to dotfiles directory
-export UV_LINK_MODE=copy         # Symlink mode for UV
-export BAT_THEME="Dracula"       # Syntax highlighting theme
-export BUN_INSTALL="$HOME/.bun"  # Bun installation directory
-export NVM_DIR="$HOME/.nvm"      # NVM installation directory
+export SHELL=/bin/zsh                  # Shell name
+export DOTFILES="$HOME/dotfiles"       # Path to dotfiles directory
+export UV_LINK_MODE=copy               # Symlink mode for UV
+export BAT_THEME="Dracula"             # Syntax highlighting theme
+export BUN_INSTALL="$HOME/.bun"        # Bun installation directory
+export NVM_DIR="$HOME/.nvm"            # NVM installation directory
+export PNPM_HOME="$HOME/.local/share/pnpm"   # PNPM home
 
 # --- Completions & Loaders ---
 # Load Bun completions
 [[ -s "$BUN_INSTALL/_bun" ]] && source "$BUN_INSTALL/_bun"
 
 # Load NVM and its completions
-[[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"                   # Main script
-[[ -s "$NVM_DIR/bash_completion" ]] && . "$NVM_DIR/bash_completion" # Shell completion
+[[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"
+[[ -s "$NVM_DIR/bash_completion" ]] && . "$NVM_DIR/bash_completion"
 
 # --- Path Management ---
-typeset -U path # Prevent duplicate entries in PATH
+typeset -U path   # Prevent duplicate PATH entries
 
 path=(
-  "$BUN_INSTALL/bin"             # Add Bun binaries
-  $HOME/{go/bin,.local/bin}(N-/) # Add Go and local binaries (if exist)
-  $path                          # Append existing paths
+  "$PNPM_HOME"                 # pnpm global bin (IMPORTANT)
+  "$BUN_INSTALL/bin"           # Bun binaries
+  $HOME/{go/bin,.local/bin}(N-/)
+  $path
 )
 
-export PATH # Sync to environment
+export PATH
