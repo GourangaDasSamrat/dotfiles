b94c19a9c0301112fee529c3d1fc6d5696c6c65f
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-05-03 22:04:03 +0600

    feat(env): add Go and Cargo env vars
    - Added GOPATH and CARGO_NET_GIT_FETCH_WITH_CLI
    - Updated PATH to include GOPATH/bin

--- zsh/.config/zsh/core/env.zsh
+++ zsh/.config/zsh/core/env.zsh
@@ -1,3 +1,13 @@
+# --- Termux Specific Environment Variables ---
+if [[ -d /data/data/com.termux ]]; then
+   export SSL_CERT_FILE=/data/data/com.termux/files/usr/etc/tls/cert.pem # Path to SSL certificates
+fi
+
+# --- Editor & Pager ---
+export EDITOR="hx"                           # Set Helix as default editor
+export VISUAL="$EDITOR"                      # Set Visual editor
+export PAGER="less"                          # Default pager for long outputs
+
 # --- Environment Variables ---
 export DOTFILES="$HOME/dotfiles"             # Path to dotfiles directory
 export UV_LINK_MODE=copy                     # Symlink mode for UV
@@ -5,6 +15,8 @@ export BAT_THEME="Dracula"                   # Syntax highlighting theme
 export BUN_INSTALL="$HOME/.bun"              # Bun installation directory
 export NVM_DIR="$HOME/.nvm"                  # NVM installation directory
 export PNPM_HOME="$HOME/.local/share/pnpm"   # PNPM home
+export GOPATH="$HOME/go"                     # Go workspace directory
+export CARGO_NET_GIT_FETCH_WITH_CLI=true     # Use system git for better auth/network stability
 
 # --- Completions & Loaders ---
 # Load Bun completions
@@ -16,12 +28,13 @@ export PNPM_HOME="$HOME/.local/share/pnpm"   # PNPM home
 # --- Path Management ---
 typeset -U path   	       # Prevent duplicate PATH entries
 path=(
-  "$PNPM_HOME"                 # pnpm global bin (IMPORTANT)
+  "$PNPM_HOME"                 # pnpm global bin
   "$BUN_INSTALL/bin"           # Bun binaries
   "/opt/homebrew/bin"          # Homebrew binaries (Apple Silicon)
   "/opt/homebrew/sbin"         # Homebrew sbin (Apple Silicon)
   "$HOME/.cargo/bin"           # Rust/Cargo binaries
-  $HOME/{go/bin,.local/bin}(N-/)
+  "$GOPATH/bin"                # Go binaries using
+  $HOME/{.local/bin}(N-/)      # Local user binaries
   $path
 )
 export PATH
