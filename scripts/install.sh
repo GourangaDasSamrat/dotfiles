beae7b8634c00aa3f648d2605af5901689964456
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-06-09 15:15:32 +0600

    fix(scripts): rename packages script to softwares

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-source "$SCRIPT_DIR/lib/packages.sh"
+source "$SCRIPT_DIR/lib/softwares.sh"
 source "$SCRIPT_DIR/lib/plugins.sh"
 source "$SCRIPT_DIR/lib/wallpapers.sh"
 source "$SCRIPT_DIR/lib/dotfiles.sh"
