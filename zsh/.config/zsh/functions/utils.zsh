9fb8bb8fd0a94de110f7a80dc371465ad7457519
Gouranga Das Samrat <gouranga.das.khulna@gmail.com>
2026-09-23 16:57:35 +0600

    feat(zsh): add package manager wrappers
    - Wrap apt, brew, dnf for simple install/remove
    - Improve CLI experience for package management

--- zsh/.config/zsh/functions/utils.zsh
+++ zsh/.config/zsh/functions/utils.zsh
@@ -244,3 +244,30 @@ gensalt() {
 	_gen_random "$bytes" "$mode" || return 1
 	_ok "Salt (${bytes}B, $mode): ${COLOR_CURSOR}$REPLY${COLOR_RESET}"
 }
+
+# Generic wrapper: cmd, install-flag/subcmd, remove-flag/subcmd
+_pkg_wrapper() {
+	local cmd=$1 install=$2 remove=$3
+	(( $+commands[$cmd] )) || return
+
+	eval "
+	$cmd() {
+		if [[ \$1 == i ]]; then
+			shift
+			command $cmd $install \"\$@\"
+		elif [[ \$1 == rm ]]; then
+			shift
+			command $cmd $remove \"\$@\"
+		else
+			command $cmd \"\$@\"
+		fi
+	}
+	"
+	compdef _$cmd $cmd 2>/dev/null
+}
+
+_pkg_wrapper apt  install remove
+_pkg_wrapper brew install uninstall
+_pkg_wrapper dnf  install remove
+
+unfunction _pkg_wrapper
